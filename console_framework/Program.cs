fb74975531e9876c32435c18f9091cc9114bbf16
ares128 <ares128@gmail.com>
2017-09-26 06:42:13 +0800

    Add project files.

---
+++ console_framework/Program.cs
@@ -0,0 +1,30 @@
+﻿using ef_sqlite3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace console_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
+        }
+    }
+}
